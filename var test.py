2ae80160731de99879ccb28c857984f117e40d18
ZT <gaogaogaosorgar@protonmail.com>
2022-05-04 03:52:38 +0700

    further messing around refactored variable names to be more sensical

--- var test.py
+++ var test.py
@@ -1,17 +1,33 @@
-testVariable = 50
-testVariable2 = 55
-testVariableMult = 2
+from math import factorial
 
-testVariable3 = str(testVariable2)
-testVariable4 = int(testVariable3)
-testVariable5 = testVariable3
-testVariable6 = int(testVariable5+testVariable3)
+testVarBase = 50
+testVarBase2 = 55
+testVarMult = 2 #2 factorial less interesting
+testVarMult = 4
+testVarBool = True
 
-print(testVariable)
-print(testVariable2 + testVariable)
-print(testVariable3)
-print(testVariable3 + str(testVariable2))
-print(testVariable4 + testVariable)
-print(testVariable5)
-print(testVariable6)
-print(testVariable6*testVariableMult)
+
+testVarB2_String = str(testVarBase2)
+testVar_StrToInt = int(testVarB2_String)
+testVarB2_dupe = testVarB2_String
+testVar_Concat = int(testVarB2_dupe + testVarB2_String)
+testVarMultFactorial = factorial(testVarMult)
+testVar_ConcatFloat = float((testVarB2_String+"."+testVarB2_String))
+
+print(testVarBase)
+print(testVarBase2 + testVarBase)
+print(testVarB2_String)
+print(testVarB2_String + str(testVarBase2))
+print(testVar_StrToInt + testVarBase)
+print(testVarB2_dupe)
+print(testVar_Concat)
+print(testVar_Concat * testVarMult)
+#print(testVariable3*testVariable5)
+print(testVarBase2 / testVarBase)
+print((testVarBase2 / testVarBase) + testVarMult)
+#print(testVariableMult!)
+print(testVarBase % testVarMult)
+print(str(testVarBool) + testVarB2_String)
+print(testVarMultFactorial)
+print(2 * testVarBase)
+print(testVar_ConcatFloat*testVarMult)
