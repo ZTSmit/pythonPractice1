ef740a9f6d83da3eeccb08c815dea42882b659a6
ZT <gaogaogaosorgar@protonmail.com>
2022-05-04 03:24:30 +0700

    testing w varibales

---
+++ var test.py
@@ -0,0 +1,17 @@
+testVariable = 50
+testVariable2 = 55
+testVariableMult = 2
+
+testVariable3 = str(testVariable2)
+testVariable4 = int(testVariable3)
+testVariable5 = testVariable3
+testVariable6 = int(testVariable5+testVariable3)
+
+print(testVariable)
+print(testVariable2 + testVariable)
+print(testVariable3)
+print(testVariable3 + str(testVariable2))
+print(testVariable4 + testVariable)
+print(testVariable5)
+print(testVariable6)
+print(testVariable6*testVariableMult)
