fb92334942bcbc3855852cac932f4dc889c2cf70
ZT <gaogaogaosorgar@protonmail.com>
2022-05-09 04:53:28 +0700

    trying with the try and except commands on a calculator

---
+++ input test.py
@@ -0,0 +1,4 @@
+varName = "Blank"
+print(varName)
+varName = input("Enter word: ")
+print(varName)
--- var test.py
+++ var test.py
@@ -1,10 +1,13 @@
-from math import factorial
+from math import *
 
 testVarBase = 50
 testVarBase2 = 55
+testVarFloat = 135.56
 #testVarMult = 2 #2 factorial less interesting
 testVarMult = 4
+testVarOne = 1
 testVarBool = True
+testList = ["One", "Two", "Three", 4]
 
 
 testVarB2_String = str(testVarBase2)
@@ -13,6 +16,12 @@ testVarB2_dupe = testVarB2_String
 testVar_Concat = int(testVarB2_dupe + testVarB2_String)
 testVarMultFactorial = factorial(testVarMult)
 testVar_ConcatFloat = float((testVarB2_String+"."+testVarB2_String))
+testVarBoolToInt = int(testVarBool)
+testVarIntToBool = bool(testVarOne)
+
+testVarNesting = str(int(testVarBool))+str(-int(testVarBool))+str(testVarBool)
+
+testVarStringIndexing = "Utter Netter"
 
 print(testVarBase)
 print(testVarBase2 + testVarBase)
@@ -31,3 +40,10 @@ print(str(testVarBool) + testVarB2_String)
 print(testVarMultFactorial)
 print(2 * testVarBase)
 print(testVar_ConcatFloat*testVarMult)
+print(testVarBoolToInt)
+print(testVarStringIndexing.index("t"))
+print(testVarNesting)
+print(int(testVarFloat))
+print(testVarBoolToInt)
+print(testList[2])
+print(testList)
