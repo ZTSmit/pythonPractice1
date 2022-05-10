7edecf818e0c8a04029a5526f1eec3bd0549e012
ZT <gaogaogaosorgar@protonmail.com>
2022-05-11 04:57:33 +0700

    converted the tkinter_test.py to be a front end for fibonacci_test.py, added code comments to the tests

--- fibonacci_test.py
+++ fibonacci_test.py
@@ -1,35 +1,36 @@
-
 def fibonacci_check(num_check):
+    test_statement = ""  # Placeholder value
 
-    test_value = True
+    test_value = False  # Placeholder value
 
-    fib_list = [1, 1]
+    fib_list = [1, 1]  # The basic numbers where all fibonacci sequences are formed
 
-    try:
+    try:  # Checks if the input can even be converted to a string
         num_check = int(num_check)
     except ValueError:
-        print("Not a number")
-        return
+        test_statement = "Not a number"
+        print(test_statement)
+        return test_statement
 
     x = 0
-    while x > -1:
-    # for x in range(200):
-        if num_check == 2 or num_check == 1 or num_check == fib_list[x]:
+    while x > -1:  # Functionally infinite loop but should always break out
+        if num_check == 2 or num_check == 1 or num_check == fib_list[x]:  # 1 and 2 are values in a fib. sequence
             test_value = True
             break
-        elif num_check > 2 and num_check > fib_list[x]:
-            fib_list.append(fib_list[x] + fib_list[x+1])
-            print(fib_list)
-            x = x+1
+        elif num_check > 2 and num_check > fib_list[x]:  # Creates a fib. sequence from the starting list
+            fib_list.append(fib_list[x] + fib_list[x + 1])
+            # print(fib_list)
+            x = x + 1
             continue
-        elif num_check < fib_list[x]:
-            test_value = False
+        elif num_check < fib_list[x]:   # If the number is higher than the latest iteration in a fibonacci sequence
+            test_value = False          # and still has not been found, it means it is not in the fibonacci sequence
             break
 
     if test_value:
-        print(str(num_check) + " is in a fibonnaci sequence")
+        test_statement = str(num_check) + " is in a fibonnaci sequence"
+        print(test_statement)
+        return test_statement
     else:
-        print(str(num_check) + " is not in a fibonacci sequence")
-
-
-fibonacci_check(2)
+        test_statement = str(num_check) + " is not in a fibonacci sequence"
+        print(test_statement)
+        return test_statement
--- tkinter_test.py
+++ tkinter_test.py
@@ -1,10 +1,45 @@
 from tkinter import *
-import 
+from fibonacci_test import *
+
+
+def fibby():
+    fibGet = fibEntry.get()
+    fibbyResult = fibonacci_check(fibGet)
+    fibColor = 0
+    fibColorInt = 0
+    try:
+        fibColor = int(fibGet)
+    except ValueError:
+        fibColor = 4090
+
+    fibColorInt = fibColor      # This Block of code tries to convert the value given to a hexadecimal
+    fibColor = hex(fibColor)    # number to make it applicable for changing the color of the label
+    fibColor = str(fibColor)    # that the command creates
+    fibColor = fibColor.replace("0x", "")
+    if len(fibColor) > 3:               # The two if statements cut off the last digits or fill the first digits
+        tempLength = len(fibColor) - 3  # with zeroes
+        fibColor = fibColor[:-tempLength]
+    if len(fibColor) < 3:
+        tempLength = 3 - len(fibColor)
+        while tempLength > 0:
+            fibColor = "0" + fibColor
+            tempLength -= 1
+
+    fibColor = "#" + fibColor
+
+    print(fibColor)  # Checks the color in console
+
+    fibLabel.config(text=fibbyResult, bg=fibColor, foreground="#888")  # Modifies the placeholder label
+
 
 root = Tk()
 
-myLabel = Label(root, text="Hello world")
+fibLabel = Label(root, text="Placeholder")
+fibEntry = Entry(root, bg="#FFA")
+myButton = Button(root, text="Click to check!", command=fibby, bg="#FFE")  # Placeholder values for the label
 
-myLabel.pack()
+fibLabel.grid(row=1, column=2)
+myButton.grid(row=99, column=2)
+fibEntry.grid(row=1, column=1)
 
 root.mainloop()
